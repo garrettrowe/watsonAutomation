40e5721c6d7970904eb7271f0de2621fbc3b258b
Garrett Rowe <garrettrowe@yahoo.com>
2021-01-21 12:12:42 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 variable "ibmcloud_api_key" {}
-variable "iaas_classic_username" {}
-variable "iaas_classic_api_key" {}
+#variable "iaas_classic_username" {}
+#variable "iaas_classic_api_key" {}
 
 terraform {
   required_providers {
@@ -15,6 +15,6 @@ provider "ibm" {
   ibmcloud_api_key   = var.ibmcloud_api_key
   generation         = 2
   region             = "us-south"
-  iaas_classic_username = var.iaas_classic_username
-  iaas_classic_api_key  = var.iaas_classic_api_key
+#  iaas_classic_username = var.iaas_classic_username
+#  iaas_classic_api_key  = var.iaas_classic_api_key
 }
