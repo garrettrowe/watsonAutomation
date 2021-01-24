294c5c31857c3a4d4b1e7c25d4086016bd45e501
Garrett Rowe <garrettrowe@yahoo.com>
2021-01-24 10:04:01 -0800

    Update wa.tf

--- wa.tf
+++ wa.tf
@@ -4,9 +4,7 @@ data "local_file" "configs" {
 
 locals {
     instnum = regex("([^\\.][a-zA-Z]*-watsonA\\w+)", data.local_file.configs.content)
-}
-locals {
-    company = regex("[a-zA-Z0-9_ ]+", local.instnum)
+    company = regex("[a-zA-Z0-9_ ]+", local.instnum[0])
 }
 
 
