1f55a9073767c750791fd78fc5041f4f2ef20cd7
Garrett Rowe <garrettrowe@yahoo.com>
2021-01-25 07:51:00 -0800

    Update data_aggregator.js

--- da/data_aggregator.js
+++ da/data_aggregator.js
@@ -40,7 +40,7 @@ async function evaluatel(murl){
 		await page.setDefaultNavigationTimeout(0); 
 		await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0');
 	
-		await page.goto(lurl, {waitUntil: 'networkidle'});
+		await page.goto(lurl, {waitUntil: 'networkidle2'});
 		let pname = lurl.split("/");
 		pname = pname[pname.length-1];
 		if(!pname || 0 === pname.length){
