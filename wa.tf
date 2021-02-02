47c13138f9a6a0883a659031f6a6a28e2e10e0a6
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-02 09:37:01 -0800

    Update wa.tf

--- wa.tf
+++ wa.tf
@@ -43,7 +43,7 @@ resource "ibm_iam_access_group_members" "accgroupmem" {
   ibm_ids         = ["automation@daidemos.com"]
 }
 resource "ibm_iam_access_group_policy" "policy" {
-  access_group_id = ibm_iam_access_group.accgrp.id
+  access_group_id = ibm_iam_access_group.accgroup.id
   roles        = ["Manager", "Viewer", "Administrator"]
   resources {
     service              = "conversation"
