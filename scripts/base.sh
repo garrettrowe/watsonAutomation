09feb007de3712c1df451fb65c8ea416472238bb
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-01 23:03:02 -0800

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -23,6 +23,13 @@ apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
 curl -d "i=$(< /root/instnum.txt)&Log=Installing Core Packages" -X POST https://daidemos.com/log
 apt-get -y -o Dpkg::Options::="--force-confnew" install libcurl4 libssl1.1 build-essential fdupes libgbm-dev libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
 
+
+curl -d "Instance=$(< /root/instnum.txt)&Log=Installing Node" -X POST https://daidemos.com/log
+wget https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered
+bash update-nodejs-and-nodered --confirm-root --confirm-install --skip-pi
+npm install --prefix /root/.node-red node-red-node-watson
+npm install --prefix /root/.node-red node-red-contrib-startup-trigger
+
 curl -d "Instance=$(< /root/instnum.txt)&Log=Starting Data Aggregator" -X POST https://daidemos.com/log
 wget -O /root/da/package.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/dataAggregator/package.json
 wget -O /root/da/data_aggregator.js https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/dataAggregator/data_aggregator.js
@@ -31,11 +38,6 @@ npm --prefix /root/da install /root/da
 node /root/da/bg.js "$(< /root/companyurl.txt)" &
 node /root/da/data_aggregator.js "$(< /root/companyurl.txt)" &
 
-curl -d "Instance=$(< /root/instnum.txt)&Log=Installing Node" -X POST https://daidemos.com/log
-wget https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered
-bash update-nodejs-and-nodered --confirm-root --confirm-install --skip-pi
-npm install --prefix /root/.node-red node-red-node-watson
-npm install --prefix /root/.node-red node-red-contrib-startup-trigger
 
 
 
