9c4f3b7dbba1c8fff48ce8ed258fba6407266d2e
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-02 14:44:47 -0800

    Update wa.tf

--- wa.tf
+++ wa.tf
@@ -65,13 +65,16 @@ resource "ibm_iam_user_invite" "invite_user" {
     users = ["automation@daidemos.com"]
     iam_policy {
       roles  = ["Manager", "Viewer", "Administrator"]
-      resources [{
-        service              = "conversation"
-        resource_instance_id = element(split(":",ibm_resource_instance.wa_instance.id),7)
-        },{
-        service              = "discovery"
-        resource_instance_id = element(split(":",ibm_resource_instance.discovery_instance.id),7)
-        }]
+      resources {
+          {
+          service              = "conversation"
+          resource_instance_id = element(split(":",ibm_resource_instance.wa_instance.id),7)
+          }
+          {
+          service              = "discovery"
+          resource_instance_id = element(split(":",ibm_resource_instance.discovery_instance.id),7)
+          }
+        }
       }
 }
 
