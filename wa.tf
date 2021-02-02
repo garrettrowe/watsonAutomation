5e0084fbb2a947b7deec105643fac19d553d3cb8
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-01 20:02:16 -0800

    Update wa.tf

--- wa.tf
+++ wa.tf
@@ -133,24 +133,24 @@ write_files:
     module.exports = {uiPort: process.env.PORT || 80, mqttReconnectTime: 15000, serialReconnectTime: 15000, debugMaxLength: 1000, httpAdminRoot: '/nadmin', adminAuth: {type: "credentials", users: [{username: "${local.company}", password: "$2b$08$Rx8EGoP8uZmLFzA.9S1CMebrt159MLtxRcCwfi8r27N2BbBDOPb1K", permissions: "*"}] }, logging: {console: {level: "info", } } }
    path: /root/.node-red/settings.js
 runcmd:
- - curl -d "i=${local.instnum}&log=Booting VSI" -X POST https://daidemos.com/log
+ - curl -d "Instance=${local.instnum}&Log=Booting VSI" -X POST https://daidemos.com/log
  - mkdir /root/demo
  - mkdir /root/da
  - export DEBIAN_FRONTEND=noninteractive
  - export HOME=/root
  - export USER=root
  - apt-get update
- - curl -d "i=${local.instnum}&log=Patching VSI" -X https://daidemos.com/log
+ - curl -d "Instance=${local.instnum}&Log=Patching VSI" -X https://daidemos.com/log
  - apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
- - curl -d "i=${local.instnum}&log=Installing Core Packages" -X POST https://daidemos.com/log
+ - curl -d "i=${local.instnum}&Log=Installing Core Packages" -X POST https://daidemos.com/log
  - apt-get -y -o Dpkg::Options::="--force-confnew" install libcurl4 libssl1.1 build-essential fdupes
- - curl -d "i=${local.instnum}&log=Installing Node" -X POST https://daidemos.com/log
+ - curl -d "Instance=${local.instnum}&Log=Installing Node" -X POST https://daidemos.com/log
  - wget https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered
  - bash update-nodejs-and-nodered --confirm-root --confirm-install --skip-pi
  - npm install --prefix /root/.node-red node-red-node-watson
  - npm install --prefix /root/.node-red node-red-contrib-startup-trigger
  - wget -O /root/.node-red/flows_${local.companysafe}-vsi.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/flows.json
- - curl -d "i=${local.instnum}&log=Starting Data Aggregator" -X POST https://daidemos.com/log
+ - curl -d "Instance=${local.instnum}&Log=Starting Data Aggregator" -X POST https://daidemos.com/log
  - apt-get install -y -o Dpkg::Options::="--force-confnew" libgbm-dev libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
  - wget -O /root/companylogo.png https://daidemos.com/${local.company}.png
  - wget -O /root/companyurl.txt https://daidemos.com/${local.company}.txt
@@ -158,11 +158,11 @@ runcmd:
  - wget -O /root/da/data_aggregator.js https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/dataAggregator/data_aggregator.js
  - wget -O /root/da/bg.js https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/dataAggregator/bg.js
  - npm --prefix /root/da install /root/da
- - curl -d "i=${local.instnum}&log=Starting Services" -X POST https://daidemos.com/log
+ - curl -d "Instance=${local.instnum}&Log=Starting Services" -X POST https://daidemos.com/log
  - systemctl enable nodered.service
  - systemctl start nodered.service
- - curl -d "i=${local.instnum}&log=Provision complete! Content loading and model training will occur in the background over the next few hours." -X POST https://daidemos.com/log
- - curl -d "i=${local.instnum}" -X POST https://daidemos.com/complete
+ - curl -d "Instance=${local.instnum}&Log=Provision complete! Content loading and model training will occur in the background over the next few hours." -X POST https://daidemos.com/log
+ - curl -d "Instance=${local.instnum}" -X POST https://daidemos.com/complete
 EOT
 }
 data "logship" "instancelog" {
