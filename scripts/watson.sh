4614e598b9fa58cdc105e81bad73e31e87401b54
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-04 23:03:35 -0800

    Update watson.sh

--- scripts/watson.sh
+++ scripts/watson.sh
@@ -21,7 +21,7 @@ done
 curl -d "Instance=$(< /root/instnum.txt)&Log=Localizing: $(< /root/industry.txt)/$(< /root/demo.txt) " -X POST https://daidemos.com/log
 wget -O /root/.node-red/flows_$(< /root/companysafe.txt)-vsi.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/flows.json
 wget -O /root/assistant.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/assistant_$(< /root/industry.txt).json
-sed -i 's/948376593648263452/$(/sbin/ip -o -4 addr list eth1 | awk '{print $4}' | cut -d/ -f1)/g' /root/assistant.json
+sed -i 's/948376593648263452/'$(curl -s ipinfo.io/ip)'/g' /root/assistant.json
 
 curl -d "Instance=$(< /root/instnum.txt)&Log=Starting Services" -X POST https://daidemos.com/log
 systemctl enable nodered.service
