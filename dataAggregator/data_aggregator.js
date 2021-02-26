3193ca5c90ccf64d36638d31a232048e555c27a5
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-26 12:12:13 -0800

    Update data_aggregator.js

--- dataAggregator/data_aggregator.js
+++ dataAggregator/data_aggregator.js
@@ -9,6 +9,68 @@ var browser = null;
 var otp = null;
 var cont = null;
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+	
+var crawler = new Crawler(myArgs[0]);
+crawler.maxDepth = 4;
+crawler.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0";
+crawler.respectRobotsTxt = false;
+crawler.allowInitialDomainChange = true;
+crawler.scanSubdomains = true;
+crawler.ignoreWWWDomain = true;
+crawler.downloadUnsupported = false;
+crawler.supportedMimeTypes = [];
+crawler.interval = 1000; 
+crawler.maxConcurrency = 1;
+crawler.listenerTTL = 120000;
+crawler.allowedProtocols ["http","https"];
+
+var gettingPage = false
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+crawler.on("fetchstart", function(queueItem, responseBuffer, response) {
+	cont = this.wait();
+	if(gettingPage){
+		queueItem.status = "queued";
+		return;
+	}
+    console.log("doing fetch: " + queueItem.url);
+	 getPandL(queueItem.url, cont).then(outp => {
+	 		if(outp){
+	            outp.forEach(function (item, index) {
+	              crawler.queueURL(item);
+	            });
+	            console.log("added: " + outp.length + ", queue length: "+ crawler.queue.length);
+	           }
+	    }).catch((err) => {console.error(err); });   
+});
+
+crawler.on("complete", function () {
+	setInterval(function(){ 
+		crawler.queue.countItems({
+		    status: "queued"
+		}, function(error, items) {
+		    if(items && !crawler.running)
+		    	crawler.start();
+		});
+	}, 5000);
+    setTimeout(function(){ 
+    	crawler.queue.countItems({
+		    status: "queued"
+		}, function(error, items) {
+			console.log("Final Check: " + items);
+		    if(items && !crawler.running)
+		    	crawler.start();
+		    else
+		    	otp = true;
+		});
+    }, 240000);   
+});
+
+
 async function launchBrowser(){
 	try {
 		const browser = await puppeteer.launch({
@@ -52,64 +114,6 @@ async function setGettingPage(gp) {
     gettingPage = gp;
 }
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-	
-var crawler = new Crawler(myArgs[0]);
-crawler.maxDepth = 4;
-crawler.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0";
-crawler.respectRobotsTxt = 0;
-crawler.allowInitialDomainChange = 1;
-crawler.scanSubdomains = 1;
-crawler.ignoreWWWDomain = 0;
-crawler.downloadUnsupported = 0;
-crawler.supportedMimeTypes = [];
-crawler.interval = 1000; 
-crawler.maxConcurrency = 1;
-crawler.listenerTTL = 120000;
-var gettingPage = false
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-crawler.on("fetchstart", function(queueItem, responseBuffer, response) {
-	cont = this.wait();
-	if(gettingPage){
-		queueItem.status = "queued";
-		return;
-	}
-    console.log("doing fetch: " + queueItem.url);
-	 getPandL(queueItem.url, cont).then(outp => {
-	 		if(outp){
-	            outp.forEach(function (item, index) {
-	              crawler.queueURL(crawler.processURL(item));
-	            });
-	            console.log("added: " + outp.length + ", queue length: "+ crawler.queue.length);
-	           }
-	    }).catch((err) => {console.error(err); });   
-});
-
-crawler.on("complete", function () {
-	setInterval(function(){ 
-		crawler.queue.countItems({
-		    status: "queued"
-		}, function(error, items) {
-		    if(items && !crawler.running)
-		    	crawler.start();
-		});
-	}, 5000);
-    setTimeout(function(){ 
-    	crawler.queue.countItems({
-		    status: "queued"
-		}, function(error, items) {
-			console.log("Final Check: " + items);
-		    if(items && !crawler.running)
-		    	crawler.start();
-		    else
-		    	otp = true;
-		});
-    }, 240000);   
-});
-
 
 async function getPandL(url, cont, gettingPage){
 	try {
@@ -117,7 +121,6 @@ async function getPandL(url, cont, gettingPage){
 			console.log("processing: " + url);
     		let page = await getPage(browser).catch((err) => {console.log(err); });
 			await page.goto(url, {waitUntil: 'networkidle0'}).catch((err) => {});
-	//	await page.screenshot({path: 'purl.png'});
 			
 			let pageTitle = await page.title().catch((err) => {});
 			pageTitle = pageTitle.replace(/[-_|\#\@\!\%\^\&\*\(\)\<\>\[\]\{\}]+/gi," ");
@@ -144,7 +147,7 @@ async function getPandL(url, cont, gettingPage){
 				var out = out.replace(/<script([\S\s]*?)>([\S\s]*?)<\/script>/gi, "");
 				var out = out.replace(/<!--([\S\s]*?)-->/gi, "");
 				var out = out.replace(/&[a-z]+;/g, "");
-				var out = "." + out.replace(/<.\w*[^>]*>/gi, ".");
+				var out = "." + out.replace(/<.\w*[^>]*>/gi, ".").trim();
 				var out = out.replace(/\.[\w \\/]{0,80}(?=\.)/gi, ".");
 				var out = out.replace(/( )+/gi, " ");
 				var out = out.replace(/([\t\n])+/gi, ".");
@@ -196,7 +199,7 @@ function hashCode(str) {
   for (i = 0; i < str.length; i++) {
     chr   = str.charCodeAt(i);
     hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
+    hash |= 0; 
   }
   return hash;
 }
