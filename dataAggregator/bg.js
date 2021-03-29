ccfb1460e8eda6cd89045336a44a498896f615d5
Garrett Rowe <garrettrowe@yahoo.com>
2021-03-29 12:50:44 -0700

    Update bg.js

--- dataAggregator/bg.js
+++ dataAggregator/bg.js
@@ -11,10 +11,11 @@ var myArgs = process.argv.slice(2);
 		  height: 925,
 		  deviceScaleFactor: 2,
 		});
-	await page.setDefaultNavigationTimeout(0); 
-	await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0');
-
-	await page.goto(myArgs[0]);
-	await page.screenshot({path: '/root/site.png'});
-	await browser.close();
+	await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0').catch((err) => {});
+	await page.goto(myArgs[0]).catch((err) => {});
+	await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'}).catch((err) => {});
+	await page.addScriptTag({url: 'https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/dataAggregator/popBreaker.js'}).catch((err) => {});
+	await page.waitForNavigation({waitUntil: 'networkidle2'}).catch((err) => {});
+	await page.screenshot({path: 'site.png'}).catch((err) => {});
+	await browser.close().catch((err) => {});
 })();
