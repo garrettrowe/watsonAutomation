b0f843d5973452efd779b9543b47d8c321dbc61b
Garrett Rowe <garrettrowe@yahoo.com>
2021-03-29 20:36:11 -0700

    Update popBreaker.js

--- dataAggregator/popBreaker.js
+++ dataAggregator/popBreaker.js
@@ -1,8 +1,14 @@
 var allDivs = $('div div');
 var topZindex = 100;
+var targetRoles = ["dialog","modal","alert","alertdialog"];
 allDivs.each(function(){
     var currentZindex = parseInt($(this).css('z-index'), 10);
     if(currentZindex > topZindex) {
-        $(this).css('z-index',-1)
+        $(this).hide();
+        return true;
+    }
+    if(targetRoles.includes($(this).attr("role"))) {
+        $(this).hide();
+        return true;
     }
 });
