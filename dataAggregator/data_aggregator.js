ce7c3d11e5a1d2d8aa3ce714970d8b4fe9e6189d
Garrett Rowe <garrettrowe@yahoo.com>
2021-04-09 09:45:32 -0700

    Update data_aggregator.js

--- dataAggregator/data_aggregator.js
+++ dataAggregator/data_aggregator.js
@@ -15,7 +15,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 var crawler = new Crawler(myArgs[0].replace(/^http:\/\//i, "https://"));
 crawler.maxDepth = 4;
-crawler.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36";
+crawler.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0";
 crawler.respectRobotsTxt = false;
 crawler.allowInitialDomainChange = true;
 crawler.scanSubdomains = true;
@@ -116,6 +116,49 @@ async function getPage(){
 			});
 		await page.setDefaultNavigationTimeout(8000); 
 		await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0");
+		// Pass the Webdriver Test.
+		  await page.evaluateOnNewDocument(() => {
+		    Object.defineProperty(navigator, 'webdriver', {
+		      get: () => false,
+		    });
+		  });
+
+		  // Pass the Chrome Test.
+		  await page.evaluateOnNewDocument(() => {
+		    // We can mock this in as much depth as we need for the test.
+		    window.navigator.chrome = {
+		      runtime: {},
+		      // etc.
+		    };
+		  });
+
+		  // Pass the Permissions Test.
+		  await page.evaluateOnNewDocument(() => {
+		    const originalQuery = window.navigator.permissions.query;
+		    return window.navigator.permissions.query = (parameters) => (
+		      parameters.name === 'notifications' ?
+			Promise.resolve({ state: Notification.permission }) :
+			originalQuery(parameters)
+		    );
+		  });
+
+		  // Pass the Plugins Length Test.
+		  await page.evaluateOnNewDocument(() => {
+		    // Overwrite the `plugins` property to use a custom getter.
+		    Object.defineProperty(navigator, 'plugins', {
+		      // This just needs to have `length > 0` for the current test,
+		      // but we could mock the plugins too if necessary.
+		      get: () => [1, 2, 3, 4, 5],
+		    });
+		  });
+
+		  // Pass the Languages Test.
+		  await page.evaluateOnNewDocument(() => {
+		    // Overwrite the `plugins` property to use a custom getter.
+		    Object.defineProperty(navigator, 'languages', {
+		      get: () => ['en-US', 'en'],
+		    });
+		  });
 		return page;
 	}catch (e) {
 		console.log(e);
@@ -142,7 +185,7 @@ async function getPandL(url, cont, gettingPage){
 		await setGettingPage(true).catch((err) => {});
 		console.log("processing: " + url);
     		let page = await getPage(browser).catch((err) => {console.log(err); });
-		await page.goto(url, {waitUntil: 'networkidle0'}).catch((err) => {console.log(err);});
+		await page.goto(url, {waitUntil: 'networkidle2'}).catch((err) => {console.log(err);});
 		
 		await page.evaluate(async() => {
 			var script = document.createElement('script');
@@ -153,9 +196,7 @@ async function getPandL(url, cont, gettingPage){
 				await new Promise(r => setTimeout(r, 500));
 		}).catch((err) => {console.log(err);});	
 		
-		links = await page.evaluate(() => {
-		  return Array.from(document.getElementsByTagName('a'), a => a.href);
-		}).catch((err) => {console.log(err);});	
+		links = await page.$$eval('a', links => links.map(a => a.href)).catch((err) => {console.log(err); });
 		
 		console.log("got: " + links.length + " at " + url);
 		var pc = await page.content().catch((err) => {console.log(err);});	
