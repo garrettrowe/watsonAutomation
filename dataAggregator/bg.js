6b7967e23ac3e496cc90a5a769d89cd104e081cb
Garrett Rowe <garrettrowe@yahoo.com>
2021-04-14 13:55:02 -0700

    Update bg.js

--- dataAggregator/bg.js
+++ dataAggregator/bg.js
@@ -11,8 +11,8 @@ var myArgs = process.argv.slice(2);
 		  height: 925,
 		  deviceScaleFactor: 2,
 		});
-	await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0').catch((err) => {});
-	await page.goto(myArgs[0], {waitUntil: 'networkidle2'}).catch((err) => {});
+	await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0').catch((err) => {console.log(err);});
+	await page.goto(myArgs[0], {waitUntil: 'networkidle2'}).catch((err) => {console.log(err);});
 	await page.evaluate(async () => {
             var script = document.createElement('script');
             script.src = "https://code.jquery.com/jquery-3.5.1.min.js";
@@ -54,10 +54,11 @@ var myArgs = process.argv.slice(2);
 						    }
 						}
 					}
-				}catch(fail){}
+				}catch(err){console.log(err);}
 			});
 		} catch(err) {}
 	    }).catch((err) => {console.log(err);});
+	await page.waitForNavigation({waitUntil: 'networkidle2'}).catch((err) => {});
 	await page.screenshot({path: '/root/site.png'}).catch((err) => {});
 	await browser.close().catch((err) => {});
 })();
