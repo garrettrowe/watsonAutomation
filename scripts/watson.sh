2f24f3a5f6648397ead59c7a4f6510fe6418daff
Garrett Rowe <garrettrowe@yahoo.com>
2021-04-15 18:56:56 -0700

    Update watson.sh

--- scripts/watson.sh
+++ scripts/watson.sh
@@ -20,9 +20,6 @@ done
 
 curl -d "Instance=$(< /root/instnum.txt)&Log=Localizing: $(< /root/industry.txt)/$(< /root/demo.txt) " -X POST https://daidemos.com/log
 wget -O /root/.node-red/flows_$(< /root/companysafe.txt)-vsi.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/flows.json
-wget -O /root/assistant.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/assistant_$(< /root/industry.txt).json
-sed -i 's/948376593648263452/'$(curl -s ipinfo.io/ip)'/g' /root/assistant.json
-sed -i 's/94729873749632/'"$(< /root/companytitle.txt)"'/g' /root/assistant.json
 
 wget -O /root/discovery.tgz https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/discovery.tgz
 wget -O /root/upsell.zip https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/upsell.zip
