967c05edd03884d433162f43fb2caade80708539
Garrett Rowe <garrettrowe@yahoo.com>
2021-04-20 19:57:23 -0700

    Update bg.js

--- dataAggregator/bg.js
+++ dataAggregator/bg.js
@@ -10,7 +10,7 @@ if (myArgs[0].search(/http.*\/\//) == -1)
 
 	const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--proxy-server=http://compound.latentsolutions.com:18888']
+        args: ['--no-sandbox']
         });
 	const page = await browser.newPage();
 	await page.setViewport({
