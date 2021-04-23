e1641e1b998a78903ff7631639f791cec73fa49f
Garrett Rowe <garrettrowe@yahoo.com>
2021-04-22 21:58:50 -0700

    Update bg.js

--- dataAggregator/bg.js
+++ dataAggregator/bg.js
@@ -1,35 +1,88 @@
 const puppeteer = require('puppeteer-extra')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
-
+let browser = null;
 var myArgs = process.argv.slice(2);
 if (myArgs[0].search(/http.*\/\//) == -1)
 	myArgs[0] = "http://" + myArgs[0];
 
-(async () => {
-
-	const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--proxy-server=http://compound.latentsolutions.com:18888', '--ignore-certificate-errors']
+async function launchBrowser() {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--ignore-certificate-errors']
         });
-	const page = await browser.newPage();
+        return browser;
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function launchHBrowser() {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--proxy-server=http://compound.latentsolutions.com:18888', '--ignore-certificate-errors']
+        });
+        return browser;
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function getPage() {
+    try {
+        if (!browser)
+            browser = await launchBrowser().catch((err) => {
+                console.error(err);
+            });
+        const page = await browser.newPage();
+        page.setDefaultNavigationTimeout(120000);
 	await page.setViewport({
 		  width: 1680,
 		  height: 925,
 		  deviceScaleFactor: 2,
 		});
-	page.setDefaultNavigationTimeout(120000);
+        return page;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+(async () => {
+
+        let page = await getPage().catch((err) => {
+            console.log(err);
+        });
 	
 	const maxRetryNumber = 10;
-	for (let retryNumber = 1; retryNumber <= maxRetryNumber; retryNumber++) {
-		const response = await page.goto(myArgs[0], {waitUntil: 'networkidle2'}).catch((err) => {console.log(err);});
-		if (response) {
-			if (response.status() < 400) {
-			    break;
-			}
-		    }
-		await new Promise(r => setTimeout(r, 5000));
-	}
+        for (let retryNumber = 1; retryNumber <= maxRetryNumber; retryNumber++) {
+            const response = await page.goto(myArgs[0], {waitUntil: 'networkidle2'}).catch((err) => {console.log(err);});
+            if (response) {
+                console.log("Response: " + response.status() );
+                if (response.status() < 400) {
+                    break;
+                }
+                if (retryNumber > 1 && response.status() == 403 ){
+                    if (page)
+                        await page.close().catch((err) => {
+                            console.error(err);
+                        });
+                    if (browser) {
+                        await browser.close().catch((err) => {
+                            console.error(err);
+                        });
+                        browser = null;
+                    }
+                    browser = await launchHBrowser().catch((err) => {
+                        console.error(err);
+                    });
+                    page = await getPage().catch((err) => {
+                        console.log(err);
+                    });
+                }
+            }
+            await new Promise(r => setTimeout(r, 5000));
+        }
 
 	await page.evaluate(() => {
             var script = document.createElement('script');
