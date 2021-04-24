7f2f6ebe181bec99f46d7c8d71a8e6e3c689b981
Garrett Rowe <garrettrowe@yahoo.com>
2021-04-24 15:44:35 -0700

    Update data_aggregator.js
    Fixed bug in JQuery injection

--- dataAggregator/data_aggregator.js
+++ dataAggregator/data_aggregator.js
@@ -216,7 +216,7 @@ async function getPandL(url) {
 
             var retries = 0;
 
-            while (!window.jQuery && retries < 10){
+            while (!$ && retries < 10){
                 retries += 1;
                 await new Promise(r => setTimeout(r, 500));
             }
