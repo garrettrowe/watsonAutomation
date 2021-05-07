31b9632a48b1a93b112a91b971dfece5eb856380
Garrett Rowe <garrettrowe@yahoo.com>
2021-05-06 21:52:15 -0700

    Update data_aggregator.js

--- dataAggregator/data_aggregator.js
+++ dataAggregator/data_aggregator.js
@@ -124,19 +124,53 @@ crawler.on("complete", function() {
 async function launchBrowser() {
     try {
         const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome',
             headless: true,
-            args: ['--no-sandbox', '--ignore-certificate-errors']
+            defaultViewport: { 2560, 1600 },
+            ignoreHTTPSErrors: true,
+            args: [   '--no-sandbox', 
+                      '--ignore-certificate-errors', 
+                      '--disable-features=IsolateOrigins,site-per-process', 
+                      '--flag-switches-begin --disable-site-isolation-trials --flag-switches-end',
+                      '--disable-accelerated-2d-canvas',
+                      '--disable-blink-features=AutomationControlled',
+                      '--disable-dev-shm-usage',
+                      '--disable-gpu',
+                      '--disable-setuid-sandbox',
+                      '--disable-web-security',
+                      '--ignore-certificate-errors',
+                      '--no-first-run',
+                      '--window-size=2560,1600'
+                  ]
         });
         return browser;
     } catch (e) {
         console.log(e);
     }
 }
+
 async function launchHBrowser() {
     try {
         const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome',
             headless: true,
-            args: ['--no-sandbox', myArgs[1], '--ignore-certificate-errors']
+            defaultViewport: { 2560, 1600 },
+	    ignoreHTTPSErrors: true,
+	    args: [   myArgs[1],
+		      '--no-sandbox', 
+		      '--ignore-certificate-errors', 
+		      '--disable-features=IsolateOrigins,site-per-process', 
+		      '--flag-switches-begin --disable-site-isolation-trials --flag-switches-end',
+		      '--disable-accelerated-2d-canvas',
+		      '--disable-blink-features=AutomationControlled',
+		      '--disable-dev-shm-usage',
+		      '--disable-gpu',
+		      '--disable-setuid-sandbox',
+		      '--disable-web-security',
+		      '--ignore-certificate-errors',
+		      '--no-first-run',
+		      '--window-size=2560,1600'
+		  ]
         });
         return browser;
     } catch (e) {
