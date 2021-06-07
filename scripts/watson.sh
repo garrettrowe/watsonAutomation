90246273d9a0ee839588bc4ad03f1a8f448736f6
Garrett Rowe <garrettrowe@yahoo.com>
2021-06-07 15:05:34 -0700

    Update watson.sh

--- scripts/watson.sh
+++ scripts/watson.sh
@@ -17,11 +17,21 @@ done
 while [ ! -f /root/demo.txt ]; do
     sleep 1
 done
+while [ ! -f /root/watsondiscoveryInst.txt ]; do
+    sleep 1
+done
 
 curl -d "Instance=$(< /root/instnum.txt)&Log=Localizing: $(< /root/industry.txt)/$(< /root/demo.txt) " -X POST https://daidemos.com/log
 wget -O /root/.node-red/flows_$(< /root/resourceGroup.txt)-vsi.json https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/flows.json
 
+dvar=`cat watsondiscoveryInst.txt | grep -c '{"discovery.version":"2'`
+if [ $dvar -gt 0 ]
+then
+wget -O /root/discovery.tgz https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/discoV2.tgz
+else
 wget -O /root/discovery.tgz https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/discovery.tgz
+fi
+
 wget -O /root/upsell.zip https://raw.githubusercontent.com/garrettrowe/watsonAutomation/main/demos/watson/upsell.zip
 tar -xvzf /root/discovery.tgz -C /root
 npm --prefix /root/discoveryService install /root/discoveryService
