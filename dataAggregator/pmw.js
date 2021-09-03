bc3b8f84a14e4721eb1241dcae937954fa7115c1
Garrett Rowe <garrettrowe@yahoo.com>
2021-09-03 15:26:30 -0700

    Update pmw.js

--- dataAggregator/pmw.js
+++ dataAggregator/pmw.js
@@ -9,7 +9,7 @@ function onProxyRes(proxyRes, req, res) {
   delete proxyRes.headers['content-security-policy'];
   delete proxyRes.headers['link'];
   delete proxyRes.headers['strict-transport-security'];
-  delete proxyRes.headers['x-frame-options'];
+  delete proxyRes.headers['X-Frame-Options'];
   delete proxyRes.headers['x-xss-protection'];
   delete proxyRes.headers['x-content-type-options'];
   delete proxyRes.headers['Access-Control-Allow-Headers'];
@@ -22,6 +22,9 @@ const options = {
   ws: true,
   selfHandleResponse: true,
   onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Content-Security-Policy", " frame-ancestors https://*.daidemos.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     const response = responseBuffer.toString('utf8'); // convert buffer to string
     return response.replace(uReg, "");
   }),
@@ -30,6 +33,7 @@ const ep = createProxyMiddleware(options);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Security-Policy", " frame-ancestors https://*.daidemos.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
