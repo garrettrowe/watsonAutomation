90e4985b92925a223478752d3adf890bfaffb88e
Garrett Rowe <garrettrowe@yahoo.com>
2021-09-03 23:05:33 -0700

    Update pmw.js

--- dataAggregator/pmw.js
+++ dataAggregator/pmw.js
@@ -5,17 +5,6 @@ const url = new URL(myArgs[0]);
 const uReg = new RegExp(url.origin, 'gim');
 const app = express();
 
-function onProxyRes(proxyRes, req, res) {
-  delete proxyRes.headers['content-security-policy'];
-  delete proxyRes.headers['link'];
-  delete proxyRes.headers['strict-transport-security'];
-  delete proxyRes.headers['X-Frame-Options'];
-  delete proxyRes.headers['x-xss-protection'];
-  delete proxyRes.headers['x-content-type-options'];
-  delete proxyRes.headers['Access-Control-Allow-Headers'];
-  proxyRes.headers['Access-Control-Allow-Origin'] = '*'; 
-}
-
 const options = {
   target: url.origin,
   changeOrigin: true,
@@ -25,6 +14,7 @@ const options = {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Content-Security-Policy", " frame-ancestors https://*.daidemos.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.statusCode = 200;
     const response = responseBuffer.toString('utf8'); // convert buffer to string
     return response.replace(uReg, "").replace(/target="_blank"/gim , "");
   }),
