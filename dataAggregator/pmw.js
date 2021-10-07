5e77b883eadbdfe9172331b7b184545e9ce02370
root <root@automationserver.automation.cloud>
2021-10-07 06:26:17 +0000

    final cleanup

--- dataAggregator/pmw.js
+++ dataAggregator/pmw.js
@@ -91,7 +91,7 @@ const options = {
         }
       });
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Content-Security-Policy", " frame-ancestors https://*.daidemos.com:*");
+      res.header("Content-Security-Policy", "default-src *  data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline'; frame-ancestors * data: blob: 'unsafe-inline';"); 
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
       var t = proxyRes.headers["location"];
